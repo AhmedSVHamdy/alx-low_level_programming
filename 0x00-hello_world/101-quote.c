110a4be221bf989ede51644fcfa8f9362067aaef
AhmedSVHamdy <ahmedmohamedtaha200444@gmail.com>
2023-03-16 15:30:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * main - A program that print a line to the standerd error
- * Return: 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
 fput("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
+}
