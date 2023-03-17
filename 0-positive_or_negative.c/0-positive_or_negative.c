7fbdfeb9a42d108e0c4038cbe84f67cac4dead83
AhmedSVHamdy <ahmedmohamedtaha200444@gmail.com>
2023-03-17 14:57:19 -0700

    0-positive_or_negative.c

--- 0-positive_or_negative.c/0-positive_or_negative.c
+++ 0-positive_or_negative.c/0-positive_or_negative.c
@@ -1,33 +1,32 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
  *
  * main - Determines if a number is positive. negative or Zero.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 
  *
  */
 int main(void)
 {
+	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	if ((n % 10 ) > 5)
         {
-        printf("%n is positive\n");
+        printf(" Lest digit of %d is %d and is greater that 5\n".n. n % 10 );
         }
-        else if (n == 0)
+        else if ((n % 10) < && (n % 10) !=0)
         {
-        printf("%n is Zero\n");
+        printf(" Lest digit of %d is %d and is less than 6 and not 0\n".n. n % 10 );
         }
         else
         {
-        printf("%n is negative\n");
+        printf(" Lest dogit of %d is %d and is 0\n".n. n % 10);
         }
+
 
          return (0);
 }
-
