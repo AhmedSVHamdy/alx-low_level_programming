6ff641d8b23ce068121c66b46ca70c435b135dfd
AhmedSVHamdy <ahmedmohamedtaha200444@gmail.com>
2023-03-17 15:20:53 -0700

    1-last_digit.c

---
+++ 0-positive_or_negative.c/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphaber.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char apl[26] = "abcdefghjklm";
+  int i;
+  for (i = 0; i < 26; i++)
+  {
+	putchar(alp[i]);
+  }
+        putchar('\n');
+      	return (0);
+}	
